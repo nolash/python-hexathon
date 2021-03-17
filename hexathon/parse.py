cc88666420f0c52fa3e870642cbb2d0c3037ac1c
nolash <dev@holbrook.no>
2021-03-17 22:31:52 +0100

    Add pad

--- hexathon/parse.py
+++ hexathon/parse.py
@@ -29,8 +29,24 @@ def strip_0x(hx):
 
 
 def add_0x(hx):
-    if len(hx) < 2:
+    if len(hx) == 0:
         raise ValueError('invalid hex')
     if hx[:2] == '0x':
         hx = hx[2:]
     return '0x' + even(hx)
+
+
+def unpad(hx):
+    hx = strip_0x(hx)
+    i = 0
+    for i in range(len(hx)):
+        if hx[i] != '0':
+            break
+    hx = hx[i:]
+    return even(hx)
+
+
+def pad(hx, byte_length):
+    hx = strip_0x(hx)
+    hx = hx.rjust(byte_length * 2, '0')
+    return hx
